831807c4e577fd95081f25c40ea2eb68c046e80f
Florence <monnaie.florence@gmail.com>
2021-04-02 16:29:27 +0200

    ve 17h30

--- resources/views/admin.blade.php
+++ resources/views/admin.blade.php
@@ -3,4 +3,33 @@
 @section('admin-content')
 
 <h1>Okiii admin</h1>
+<table class="table table-primary table-hover">
+    <thead>
+      <tr>
+        <th scope="col">#</th>
+        <th scope="col">First</th>
+        <th scope="col">Last</th>
+        <th scope="col">Handle</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr>
+        <th scope="row">1</th>
+        <td>Mark</td>
+        <td>Otto</td>
+        <td>@mdo</td>
+      </tr>
+      <tr>
+        <th scope="row">2</th>
+        <td>Jacob</td>
+        <td>Thornton</td>
+        <td>@fat</td>
+      </tr>
+      <tr>
+        <th scope="row">3</th>
+        <td colspan="2">Larry the Bird</td>
+        <td>@twitter</td>
+      </tr>
+    </tbody>
+</table>
 @endsection
--- resources/views/templates/admin.blade.php
+++ resources/views/templates/admin.blade.php
@@ -16,7 +16,7 @@
     {{-- new bootstrap and style sheet --}}
     <link rel="stylesheet" href="{{asset('css/app.css')}}">
 	{{-- old bootstrap - priority --}}
-    <link rel="stylesheet" href="{{asset('css/bootstrap.min.css')}}"/>
+    {{-- <link rel="stylesheet" href="{{asset('css/bootstrap.min.css')}}"/> --}}
 	{{-- <link rel="stylesheet" href="css/font-awesome.min.css"/> --}}
 	<link rel="stylesheet" href="{{asset('css/flaticon.css')}}"/>
 	<link rel="stylesheet" href="{{asset('css/magnific-popup.css')}}"/>
@@ -32,73 +32,163 @@
 </head>
 <body>
 	<style>
+        /*   */
 		.overlay {
 			background-image: url("{{asset('/storage/img/overlay.png')}}");
 		}
 	</style>
-	<!-- Page Preloder -->
+	{{-- <!-- Page Preloder -->
 	<div id="preloder">
 		<div class="loader">
 			<img src="/storage/img/{{$images[0]->src}}" alt="" width="111">
 			<h2>Loading.....</h2>
 		</div>
-	</div>
-    <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
-        <div class="container">
-            <a class="navbar-brand" href="{{ url('/') }}">
-                {{ config('app.name', 'Laravel') }}
-            </a>
-            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
-                <span class="navbar-toggler-icon"></span>
-            </button>
-
-            <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                <!-- Left Side Of Navbar -->
-                <ul class="navbar-nav mr-auto">
-
-                </ul>
-
-                <!-- Right Side Of Navbar -->
-                <ul class="navbar-nav ml-auto">
-                    <!-- Authentication Links -->
-                    @guest
-                        @if (Route::has('login'))
-                            <li class="nav-item">
-                                <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
-                            </li>
-                        @endif
-                        
-                        @if (Route::has('register'))
-                            <li class="nav-item">
-                                <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
-                            </li>
-                        @endif
-                    @else
-                        <li class="nav-item dropdown">
-                            <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                                {{ Auth::user()->name }}
-                            </a>
-
-                            <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
-                                <a class="dropdown-item" href="{{ route('logout') }}"
-                                   onclick="event.preventDefault();
-                                                 document.getElementById('logout-form').submit();">
-                                    {{ __('Logout') }}
-                                </a>
-
-                                <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
-                                    @csrf
-                                </form>
-                            </div>
-                        </li>
-                    @endguest
-                </ul>
+	</div> --}}
+    <main class="back-office-main  d-flex">
+        <div class="sidebar bg-dark">
+            <div class="sidebar-inner">
+                <div class="text-center ">
+                    <a href="/" class="btn btn-success text-primary font-weight-bold"> > TO the website</a>
+                </div>
+                <div class="accordion" id="linksAccordion">
+                    <div class="card bg-dark">
+                      <div class="card-header" id="headingOne">
+                        <h2 class="mb-0">
+                          <button class="btn btn-link btn-block text-left text-uppercase" type="button" data-toggle="collapse" data-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+                            Team
+                          </button>
+                        </h2>
+                      </div>
+                  
+                      <div id="collapseOne" class="collapse show" aria-labelledby="headingOne" data-parent="#linksAccordion">
+                        <div class="card-body py-4 pl-4 pr-2">
+                            <a href="" class="side-link py-3">My profile</a>
+                            <div class="side-separator my-3"></div>
+                            <a href="" class="side-link py-3">Our Team</a>
+                            <div class="side-separator my-3"></div>
+                            <a href="" class="side-link py-3">Add a Team Member</a>
+                        </div>
+                      </div>
+                    </div>
+                    <div class="card bg-dark">
+                      <div class="card-header" id="headingTwo">
+                        <h2 class="mb-0">
+                          <button class="btn btn-link btn-block text-left collapsed" type="button" data-toggle="collapse" data-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
+                            HOME
+                          </button>
+                        </h2>
+                      </div>
+                      <div id="collapseTwo" class="collapse" aria-labelledby="headingTwo" data-parent="#linksAccordion">
+                        <div class="card-body">
+                          links
+                        </div>
+                      </div>
+                    </div>
+                    <div class="card bg-dark">
+                      <div class="card-header" id="headingThree">
+                        <h2 class="mb-0">
+                          <button class="btn btn-link btn-block text-left collapsed" type="button" data-toggle="collapse" data-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
+                            SERVICES
+                          </button>
+                        </h2>
+                      </div>
+                      <div id="collapseThree" class="collapse" aria-labelledby="headingThree" data-parent="#linksAccordion">
+                        <div class="card-body">
+                          links
+                        </div>
+                      </div>
+                    </div>
+                    <div class="card bg-dark">
+                      <div class="card-header" id="headingFour">
+                        <h2 class="mb-0">
+                          <button class="btn btn-link btn-block text-left collapsed" type="button" data-toggle="collapse" data-target="#collapseFour" aria-expanded="false" aria-controls="collapseFour">
+                            BLOG
+                          </button>
+                        </h2>
+                      </div>
+                      <div id="collapseFour" class="collapse" aria-labelledby="headingFour" data-parent="#linksAccordion">
+                        <div class="card-body">
+                          links
+                        </div>
+                      </div>
+                    </div>
+                    <div class="card bg-dark">
+                        <div class="card-header" id="headingFive">
+                          <h2 class="mb-0">
+                            <button class="btn btn-link btn-block text-left" type="button" data-toggle="collapse" data-target="#collapseFive" aria-expanded="true" aria-controls="collapseFive">
+                              CONTACT
+                            </button>
+                          </h2>
+                        </div>
+                    
+                        <div id="collapseFive" class="collapse" aria-labelledby="headingFive" data-parent="#linksAccordion">
+                          <div class="card-body">
+                            links
+                          </div>
+                        </div>
+                      </div>
+                </div>
             </div>
         </div>
-    </nav>
+        <div class="main position-relative">
+        <nav class="bo-nav navbar bg-dark px-5 fixed-top">
+                @if (Auth::check())
+                <div class="d-flex align-items-center">
+                    <div class="avatar" style="background-image: url('/storage/img/team/{{Auth::user()->src}}')"></div>
+                    <a href="" class="nav-link mx-3">{{ Auth::user()->name }}</a>
+                </div>
+                @else
+                <img src="/storage/img/big-logo.png" alt="" width="111">
+                @endif
+                <ul class="navbar-nav ml-auto">
+                 <!-- Authentication Links -->
+                @guest
+                @if (Route::has('login'))
+                    <li class="nav-item">
+                        <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
+                    </li>
+                @endif
+                
+                @if (Route::has('register'))
+                    <li class="nav-item">
+                        <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
+                    </li>
+                @endif
+                @else
+                <li class="nav-item bg-dark">
+                    <a class="nav-link" href="{{ route('logout') }}"
+                        onclick="event.preventDefault();
+                                        document.getElementById('logout-form').submit();">
+                            {{ __('Logout') }}
+                    </a>
 
-    <main class="py-4">
-        @yield('admin-content')
+                    <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-nFive">
+                        @csrf
+                    </form>
+                        
+                </li>
+                @endguest
+                </ul>
+        </nav>
+            <!-- Page header -->
+        <div class="page-top-section bg-success">
+            <div class="overlay"></div>
+            <div class="container text-right">
+                <div class="page-info mr-5">
+                    <h2 class="text-capitalize text-dark">Page</h2>
+                    <div class="page-links">
+                        <a href="/" class="text-capitalize text-dark">Main</a>
+                        <span class="text-capitalize text-primary">Page</span>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="pt-5 pb-2 px-5">
+            @yield('admin-content')
+        </div>
+        
+        
+        </div>
     </main>
 
 	
